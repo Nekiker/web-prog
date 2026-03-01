64553358f0cff2697dfb74afef62f17e17f1b6e4
Nikita Morozov <mnstrelok71@gmail.com>
2026-03-02 04:50:25 +0700

    Lab8

--- sitemandalore/mandalore/models.py
+++ sitemandalore/mandalore/models.py
@@ -9,6 +9,30 @@ class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_published=PublishStatus.PUBLISHED)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+class TagPost(models.Model):
+    tag = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.tag
+
+class ShipPassport(models.Model):
+    reg_number = models.CharField(max_length=50, unique=True)
+    manufacturer = models.CharField(max_length=100, blank=True)
+    year = models.IntegerField(null=True)
+    m_count = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return f"{self.reg_number}"
+
+
 class Starship(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
@@ -18,6 +42,10 @@ class Starship(models.Model):
     is_published = models.IntegerField(choices=PublishStatus.choices, default=PublishStatus.PUBLISHED)
     objects = models.Manager()
     published = PublishedManager()
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    passport = models.OneToOneField('ShipPassport', on_delete=models.SET_NULL,
+                                    null=True, blank=True, related_name='starship')
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})
--- sitemandalore/mandalore/templatetags/mandalore_tags.py
+++ sitemandalore/mandalore/templatetags/mandalore_tags.py
@@ -1,8 +1,13 @@
 from django import template
 from mandalore import views
+from django.db.models import Count
+from mandalore.models import Category, TagPost
 
 register = template.Library()
 
+@register.inclusion_tag('mandalore/list_tags.html')
+def show_all_tags():
+    return {"tags": TagPost.objects.annotate(total=Count("tags")).filter(total__gt=0)}
 
 @register.simple_tag()
 def get_categories():
@@ -10,5 +15,5 @@ def get_categories():
 
 @register.inclusion_tag('mandalore/list_categories.html')
 def show_categories(cat_selected_id=0):
-    cats = views.cats_db
-    return {'cats': cats, 'cat_selected': cat_selected_id}
+    cats = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
+    return {"cats": cats, "cat_selected": cat_selected_id}
